bfa5e39fc6043fbae85a4876c539f2c855f3ebbd
Jose Condori <114316578+Jhosep2022@users.noreply.github.com>
2024-04-26 15:24:59 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,22 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PerfilView from '../views/PerfilView.vue'
 
+
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomeView
+    path: '/', // La ruta para la página de inicio
+    name: 'Home', // El nombre de la ruta
+    component: HomeView // El componente que se renderizará cuando se visite esta ruta
   },
   {
-    path: '/perfil',
-    name: 'Perfil',
-    component: PerfilView
+    path: '/perfil', // La ruta para la página de perfil
+    name: 'Perfil', // El nombre de la ruta
+    component: PerfilView // El componente que se renderizará cuando se visite esta ruta
   }
 ]
 
+// Creamos el enrutador con las rutas definidas y utilizando el modo de historial del navegador web
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory(), // Utilizamos el historial del navegador para una experiencia de usuario más natural
+  routes // Las rutas que definimos anteriormente
 })
 
 export default router
