38398b916e41b98e04fc37c572589e3485f8e1c2
Jose Condori <114316578+Jhosep2022@users.noreply.github.com>
2024-04-26 15:30:13 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 // App.vue
 <template>
+  <!-- Esta es la estructura HTML del componente. -->
   <div id="app">
+    <!-- router-view es un componente especial de Vue Router. -->
+    <!-- Actúa como un marcador de posición donde se renderizan los componentes de la ruta actual. -->
+    <!-- Cuando cambias de ruta, router-view se actualiza para mostrar el componente de esa ruta. -->
     <router-view/>
   </div>
 </template>
